ab5a24fcad44efd2922b1346b4a45416af613aa0
Taras Marchuk <tarlinius@gmail.com>
2019-12-01 23:15:48 +0200

    created a basic react-redux structure for project

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,19 @@
+
+export default class BookstoreService {
+
+  getBooks() {
+    return [
+      {
+        id: 1,
+        title: 'Production-Ready Microsrvices',
+        author: 'Susan J. Fowler'
+      },
+      {
+        id: 2,
+        title: 'Release It!',
+        author: 'Michael T. Nygard'
+      }
+    ];
+  }
+
+}
